aaf9e6d7a30cc8ff2295dbba4c3c962349118360
hordesk <mathias.faure.31@gmail.com>
2015-01-22 13:13:53 +0100

    Plusieur obstacles

--- app/src/main/java/fr/m2dl/japanairlines/MainActivity.java
+++ app/src/main/java/fr/m2dl/japanairlines/MainActivity.java
@@ -2,6 +2,7 @@ package fr.m2dl.japanairlines;
 
 import android.app.Activity;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -14,6 +15,10 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import fr.m2dl.japanairlines.domain.Plane;
 import fr.m2dl.japanairlines.services.BlowRecorder;
@@ -71,7 +76,8 @@ public class MainActivity extends Activity implements SensorEventListener {
         });
         t.start();
 
-        obstacle();
+        obstacle(200);
+        obstacle(100);
 
     }
 
@@ -143,7 +149,7 @@ public class MainActivity extends Activity implements SensorEventListener {
                 planeImage.setX(width-planeImage.getWidth());
             }
 
-            Log.d("",x+"--"+y);
+//            Log.d("",x+"--"+y);
             planeImage.setX(planeImage.getX()-x*5);
             planeImage.setX(planeImage.getX()-x*5);
 
@@ -161,43 +167,52 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     }
 
-    Runnable mRunnableObstalce;
 
-    public void obstacle(){
+    public void obstacle(float placement){
         final ImageView obstacle = new ImageView(this);
+        final Runnable mRunnableObstalce;
+
 
         obstacle.setImageResource(R.drawable.obstacle);
 
-        final Handler mHandlerObstacle = new Handler();
+
         mRunnableObstalce = new Runnable() {
             @Override
             public void run() {
+                if(isInBox(planeImage.getX(), planeImage.getY(), planeImage.getWidth(), planeImage.getHeight(), obstacle.getX(), obstacle.getY(),obstacle.getWidth(), obstacle.getHeight())){
+                    Log.d("","BAM");
+                }
                 obstacle.setY(obstacle.getY() + 5);
-                mHandlerObstacle.postDelayed(mRunnableObstalce, 10);
 
             }
         };
 
-        Thread t = new Thread(new Runnable() {
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
             @Override
-            public void run() {
-                affichage();
-
-                mRunnableObstalce.run();
+            public void run() { runOnUiThread(mRunnableObstalce);
             }
-        });
-        t.start();
-
-
+        };
+        timer.schedule(timerTask, 0, 10);
 
 
         mainLayout.addView(obstacle);
-        obstacle.setX(0);
+        obstacle.setX(placement);
         obstacle.setY(-300);
 
+    }
+
+    public boolean isInBox(float x1, float y1, float width1, float height1, float x2, float y2, float width2, float height2) {
 
 
+        Rect r1 = new Rect((int)x1,(int)y1,(int)x1+(int)width1,(int)y1+(int)height1);
+        Rect r2 = new Rect((int)x2,(int)y2,(int)x2+(int)width2,(int)y2+(int)height2);
 
-
+        if(r1.intersect(r2)){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
